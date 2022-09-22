1f018118fa3c7d4fabad7316c4406d5023510870
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-09-22 15:49:34 +0200

    new rect detection method under development

--- src/main.py
+++ src/main.py
@@ -21,15 +21,17 @@ class DetectOuterBorder(object):
                         ctl.ndpointer(np.uint16, flags='aligned, c_contiguous')]
         func(self.obj, array1, array2, array1.shape[0], border_x, border_y)
         return border_x, border_y
+counter = 0 
 
 
-
-
-def detect_rects(border_x, border_y, x0, y0, x1, y1, rgb_img):
+def detect_rects(border_x, border_y, x0, y0, x1, y1, rects_x_points=[], rects_y_points=[]):
     print('Detecting rectangles...')
+    global counter
+    counter = counter+1
     rects = []
     area = 0
-    thresh = 1 #border_x.shape[0]* 2/100
+    thresh = border_x.shape[0]* 2/100
+    print('thresh', thresh)
     if len(border_x) > 2000:
         step = 100
     elif len(border_x) > 1000:
@@ -38,6 +40,8 @@ def detect_rects(border_x, border_y, x0, y0, x1, y1, rgb_img):
         step = 10
     else:
         step = 1
+    border_x = np.append(border_x, rects_x_points)
+    border_y = np.append(border_y, rects_y_points)
     for i in range(1, border_x.shape[0], step):
         for ii in range(i+1, border_x.shape[0], step):
             x0_tmp = border_x[i]
@@ -47,7 +51,6 @@ def detect_rects(border_x, border_y, x0, y0, x1, y1, rgb_img):
             h_side = np.abs(float(y1_tmp) - float(y0_tmp))
             v_side = np.abs(float(x1_tmp) - float(x0_tmp))
             area_tmp = v_side*h_side
-            rgb_img_copy = rgb_img.copy()
             if area_tmp>area:
                 if x0_tmp<x1_tmp and y0_tmp<y1_tmp:
                     indexes = (border_x>=x0_tmp) & (border_x<=x1_tmp) & (border_y>=y0_tmp) & (border_y<=y1_tmp)
@@ -65,118 +68,64 @@ def detect_rects(border_x, border_y, x0, y0, x1, y1, rgb_img):
                     x1 = x1_tmp
                     y1 = y1_tmp
                     area = area_tmp
+                    print(counter, x0, y0, x1, y1, area)
     rects.append([x0, y0, x1, y1])
+    if x0<=x1 and y0<=y1:
+        x_small = x0
+        x_big = x1
+        y_small = y0
+        y_big = y1
+    elif x0<=x1 and y0>y1:
+        x_small = x0
+        x_big = x1
+        y_small = y1
+        y_big = y0
+    elif x0>x1 and y0<=y1:
+        x_small = x1
+        x_big = x0
+        y_small = y0
+        y_big = y1
+    elif x0>x1 and y0>y1:
+        x_small = x1
+        x_big = x0
+        y_small = y1
+        y_big = y0
     
-    indexes = []
+    horizontal_top_y = np.arange(y_small, y_big, 1)
+    horizontal_top_x = np.full_like(horizontal_top_y, x_small)
 
-    # # section 1
-    # border_x_tmp = border_x
-    # border_y_tmp = border_y
-    # if x0<x1 and y0<y1:
-    #     appended_border_y = np.arange(y0, y1, 1)
-    #     appended_border_x = appended_border_y*0+x0
-    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
-    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
-    #     indexes = (border_x_tmp<=x0) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
-    # elif x0<x1 and y0>y1:
-    #     appended_border_y = np.arange(y1, y0, 1)
-    #     appended_border_x = appended_border_y*0+x0
-    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
-    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
-    #     indexes = (border_x_tmp<=x0) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
-    # elif x0>x1 and y0<y1:
-    #     appended_border_y = np.arange(y0, y1, 1)
-    #     appended_border_x = appended_border_y*0+x1
-    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
-    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
-    #     indexes = (border_x_tmp<=x1) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
-    # elif x0>x1 and y0>y1:
-    #     appended_border_y = np.arange(y1, y0, 1)
-    #     appended_border_x = appended_border_y*0+x1
-    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
-    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
-    #     indexes = (border_x_tmp<=x1) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
-    # if len(indexes)>0:
-    #     border_x_tmp = border_x_tmp[indexes]
-    #     border_y_tmp = border_y_tmp[indexes]
-    #     for x,y in zip(border_x_tmp, border_y_tmp):
-    #         cv2.circle(rgb_img, (y, x), 1, (0, 0, 255), 1)
-    #     cv2.imshow('rgb_img', rgb_img)
-    #     cv2.waitKey(0)
-    #     if len(border_x_tmp) > 2:
-    #         x0 = border_x_tmp[0]
-    #         y0 = border_y_tmp[0]
-    #         x1 = border_x_tmp[1]
-    #         y1 = border_y_tmp[1]
-    #         rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
-    #         rects.extend(rects_tmp)
+    horizontal_bottom_y = np.arange(y_small, y_big, 1)
+    horizontal_bottom_x = np.full_like(horizontal_bottom_y, x_big)
 
-    # # section 2
-    # border_x_tmp = border_x
-    # border_y_tmp = border_y
-    # if x0<x1 and y0<y1:
-    #     indexes = (border_x_tmp>=x1) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
-    # elif x0<x1 and y0>y1:
-    #     indexes = (border_x_tmp>=x1) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
-    # elif x0>x1 and y0_tmp<y1:
-    #     indexes = (border_x_tmp>=x0) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
-    # elif x0_tmp>x1 and y0>y1:
-    #     indexes = (border_x_tmp>=x0) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
-    # if len(indexes)>0:
-    #         border_x_tmp = border_x_tmp[indexes]
-    #         border_y_tmp = border_y_tmp[indexes]
-    #         if len(border_x_tmp) > 2:
-    #             x0 = border_x_tmp[0]
-    #             y0 = border_y_tmp[0]
-    #             x1 = border_x_tmp[1]
-    #             y1 = border_y_tmp[1]
-    #             rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
-    #             rects.extend(rects_tmp)
+    vertical_left_x = np.arange(x_small, x_big, 1)
+    vertical_left_y = np.full_like(vertical_left_x, y_small)
 
-    # # section 3
-    # border_x_tmp = border_x
-    # border_y_tmp = border_y
-    # if x0<x1 and y0<y1:
-    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp>=y1)
-    # elif x0<x1 and y0>y1:
-    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp>=y0)
-    # elif x0>x1 and y0<y1:
-    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp>=y1)
-    # elif x0>x1 and y0>y1:
-    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp>=y0)
-    # if len(indexes)>0:
-    #     border_x_tmp = border_x_tmp[indexes]
-    #     border_y_tmp = border_y_tmp[indexes]
-    #     if len(border_x_tmp) > 2:
-    #         x0 = border_x_tmp[0]
-    #         y0 = border_y_tmp[0]
-    #         x1 = border_x_tmp[1]
-    #         y1 = border_y_tmp[1]
-    #         rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
-    #         rects.extend(rects_tmp)
+    vertical_right_x = np.arange(x_small, x_big, 1)
+    vertical_right_y = np.full_like(vertical_right_x, y_big)
 
-    # # section 4
-    # border_x_tmp = border_x
-    # border_y_tmp = border_y
-    # if x0<x1 and y0<y1:
-    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp<=y0)
-    # elif x0<x1 and y0>y1:
-    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp<=y1)
-    # elif x0_tmp>x1 and y0<y1:
-    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp<=y0)
-    # elif x0_tmp>x1 and y0>y1:
-    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp<=y1)
-    # if len(indexes)>0:
-    #     border_x_tmp = border_x_tmp[indexes]
-    #     border_y_tmp = border_y_tmp[indexes]
-    #     if len(border_x_tmp) > 2:
-    #         x0 = border_x_tmp[0]
-    #         y0 = border_y_tmp[0]
-    #         x1 = border_x_tmp[1]
-    #         y1 = border_y_tmp[1]
-    #         rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
-    #         rects.extend(rects_tmp)
+    border_x = np.append(border_x, horizontal_bottom_x)
+    border_y = np.append(border_y, horizontal_bottom_y)
+    border_x = np.append(border_x, horizontal_top_x)
+    border_y = np.append(border_y, horizontal_top_y)
+    border_x = np.append(border_x, vertical_left_x)
+    border_y = np.append(border_y, vertical_left_y)
+    border_x = np.append(border_x, vertical_right_x)
+    border_y = np.append(border_y, vertical_right_y)
+    for i in range(len(vertical_left_x)):
+        x = vertical_left_x[i]
+        y = vertical_left_y[i]
+        rect_points_y = np.arange(y, y_big)
+        rect_points_x = np.full_like(rect_points_y, x)
+        rects_x_points.append(rect_points_x)
+        rects_y_points.append(rect_points_y)
 
+    border_x = border_x.astype(np.uint16)
+    border_y = border_y.astype(np.uint16)
+    if counter>1:
+        return []
+    else:
+        rects.append(detect_rects(border_x, border_y, border_x[0], border_y[0], border_x[1], border_y[1], rects_x_points, rects_y_points))
+    
     return rects
 
 if __name__ == "__main__":
@@ -208,11 +157,7 @@ if __name__ == "__main__":
     y0 = border_y[0]
     x1 = border_x[1]
     y1 = border_y[1]
-    rects = detect_rects(border_x, border_y, x0, y0, x1, y1, rgb_img)
-    print(rects)
-    # for x,y in zip(border_x, border_y):
-    #     cv2.circle(rgb_img, (y, x), 1, (0, 0, 255), 1)
-    # cv2 rectangle
+    rects = detect_rects(border_x, border_y, x0, y0, x1, y1)
     for rect in rects:
         cv2.rectangle(rgb_img, (rect[1], rect[0]), (rect[3], rect[2]), (0, 0, 255), 1)
     cv2.imshow('image', rgb_img)
