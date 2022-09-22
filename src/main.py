e30fcf4f2dba5c35fe19ab9f13ff949c2fca2e53
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-09-22 14:14:37 +0200

    sectioning idea - not complete

--- src/main.py
+++ src/main.py
@@ -22,38 +22,24 @@ class DetectOuterBorder(object):
         func(self.obj, array1, array2, array1.shape[0], border_x, border_y)
         return border_x, border_y
 
-if __name__ == "__main__":
 
-    img_file = 'assets/images/iran_map.png'
-    rgb_img = cv2.imread(img_file)
 
-    gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
-    heigth, width = gray_img.shape
-    gray_img = 255 - gray_img
-    img_mean = np.mean(gray_img)
-    img_std = np.std(gray_img)
-    gray_img[gray_img > img_mean+img_std] = 255
-    gray_img[gray_img <= img_mean+img_std] = 0
-    
-    closing = gray_img
-    image_edges = cv2.Canny(closing, 10, 20)
 
-    [border_locations_x, border_locations_y] = np.where(image_edges == 255)
-    border_locations_x = border_locations_x.astype(np.uint16)
-    border_locations_y = border_locations_y.astype(np.uint16)
-    
-    f = DetectOuterBorder()
-    [border_x, border_y] = f.detect_border(border_locations_x, border_locations_y)
-    border_x = border_x[border_x != 0]
-    border_y = border_y[border_y != 0]
-    x0 = border_x[0]
-    y0 = border_y[0]
-    x1 = border_x[1]
-    y1 = border_y[1]
+def detect_rects(border_x, border_y, x0, y0, x1, y1, rgb_img):
+    print('Detecting rectangles...')
+    rects = []
     area = 0
-    thresh = border_x.shape[0]* 2/100
-    for i in tqdm(range(1, border_x.shape[0], 1)):
-        for ii in range(i+1, border_x.shape[0], 1):
+    thresh = 1 #border_x.shape[0]* 2/100
+    if len(border_x) > 2000:
+        step = 100
+    elif len(border_x) > 1000:
+        step = 50
+    elif len(border_x) > 500:
+        step = 10
+    else:
+        step = 1
+    for i in range(1, border_x.shape[0], step):
+        for ii in range(i+1, border_x.shape[0], step):
             x0_tmp = border_x[i]
             y0_tmp = border_y[i]
             x1_tmp = border_x[ii]
@@ -79,12 +65,156 @@ if __name__ == "__main__":
                     x1 = x1_tmp
                     y1 = y1_tmp
                     area = area_tmp
-    print(i, ii, x0, y0, x1, y1, area)
+    rects.append([x0, y0, x1, y1])
+    
+    indexes = []
 
-    for x,y in zip(border_x, border_y):
-        cv2.circle(rgb_img, (y, x), 1, (0, 0, 255), 1)
+    # # section 1
+    # border_x_tmp = border_x
+    # border_y_tmp = border_y
+    # if x0<x1 and y0<y1:
+    #     appended_border_y = np.arange(y0, y1, 1)
+    #     appended_border_x = appended_border_y*0+x0
+    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
+    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
+    #     indexes = (border_x_tmp<=x0) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
+    # elif x0<x1 and y0>y1:
+    #     appended_border_y = np.arange(y1, y0, 1)
+    #     appended_border_x = appended_border_y*0+x0
+    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
+    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
+    #     indexes = (border_x_tmp<=x0) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
+    # elif x0>x1 and y0<y1:
+    #     appended_border_y = np.arange(y0, y1, 1)
+    #     appended_border_x = appended_border_y*0+x1
+    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
+    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
+    #     indexes = (border_x_tmp<=x1) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
+    # elif x0>x1 and y0>y1:
+    #     appended_border_y = np.arange(y1, y0, 1)
+    #     appended_border_x = appended_border_y*0+x1
+    #     border_x_tmp = np.append(border_x_tmp, appended_border_x)
+    #     border_y_tmp = np.append(border_y_tmp, appended_border_y)
+    #     indexes = (border_x_tmp<=x1) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
+    # if len(indexes)>0:
+    #     border_x_tmp = border_x_tmp[indexes]
+    #     border_y_tmp = border_y_tmp[indexes]
+    #     for x,y in zip(border_x_tmp, border_y_tmp):
+    #         cv2.circle(rgb_img, (y, x), 1, (0, 0, 255), 1)
+    #     cv2.imshow('rgb_img', rgb_img)
+    #     cv2.waitKey(0)
+    #     if len(border_x_tmp) > 2:
+    #         x0 = border_x_tmp[0]
+    #         y0 = border_y_tmp[0]
+    #         x1 = border_x_tmp[1]
+    #         y1 = border_y_tmp[1]
+    #         rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
+    #         rects.extend(rects_tmp)
+
+    # # section 2
+    # border_x_tmp = border_x
+    # border_y_tmp = border_y
+    # if x0<x1 and y0<y1:
+    #     indexes = (border_x_tmp>=x1) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
+    # elif x0<x1 and y0>y1:
+    #     indexes = (border_x_tmp>=x1) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
+    # elif x0>x1 and y0_tmp<y1:
+    #     indexes = (border_x_tmp>=x0) & (border_y_tmp>=y0) & (border_y_tmp<=y1)
+    # elif x0_tmp>x1 and y0>y1:
+    #     indexes = (border_x_tmp>=x0) & (border_y_tmp>=y1) & (border_y_tmp<=y0)
+    # if len(indexes)>0:
+    #         border_x_tmp = border_x_tmp[indexes]
+    #         border_y_tmp = border_y_tmp[indexes]
+    #         if len(border_x_tmp) > 2:
+    #             x0 = border_x_tmp[0]
+    #             y0 = border_y_tmp[0]
+    #             x1 = border_x_tmp[1]
+    #             y1 = border_y_tmp[1]
+    #             rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
+    #             rects.extend(rects_tmp)
+
+    # # section 3
+    # border_x_tmp = border_x
+    # border_y_tmp = border_y
+    # if x0<x1 and y0<y1:
+    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp>=y1)
+    # elif x0<x1 and y0>y1:
+    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp>=y0)
+    # elif x0>x1 and y0<y1:
+    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp>=y1)
+    # elif x0>x1 and y0>y1:
+    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp>=y0)
+    # if len(indexes)>0:
+    #     border_x_tmp = border_x_tmp[indexes]
+    #     border_y_tmp = border_y_tmp[indexes]
+    #     if len(border_x_tmp) > 2:
+    #         x0 = border_x_tmp[0]
+    #         y0 = border_y_tmp[0]
+    #         x1 = border_x_tmp[1]
+    #         y1 = border_y_tmp[1]
+    #         rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
+    #         rects.extend(rects_tmp)
+
+    # # section 4
+    # border_x_tmp = border_x
+    # border_y_tmp = border_y
+    # if x0<x1 and y0<y1:
+    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp<=y0)
+    # elif x0<x1 and y0>y1:
+    #     indexes = (border_x_tmp<=x1) & (border_x_tmp>=x0) & (border_y_tmp<=y1)
+    # elif x0_tmp>x1 and y0<y1:
+    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp<=y0)
+    # elif x0_tmp>x1 and y0>y1:
+    #     indexes = (border_x_tmp>=x1) & (border_x_tmp<=x0) & (border_y_tmp<=y1)
+    # if len(indexes)>0:
+    #     border_x_tmp = border_x_tmp[indexes]
+    #     border_y_tmp = border_y_tmp[indexes]
+    #     if len(border_x_tmp) > 2:
+    #         x0 = border_x_tmp[0]
+    #         y0 = border_y_tmp[0]
+    #         x1 = border_x_tmp[1]
+    #         y1 = border_y_tmp[1]
+    #         rects_tmp = detect_rects(border_x_tmp, border_y_tmp, x0, y0, x1, y1)
+    #         rects.extend(rects_tmp)
+
+    return rects
+
+if __name__ == "__main__":
+
+    img_file = 'assets/images/iran_map.png'
+    rgb_img = cv2.imread(img_file)
+
+    gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
+    heigth, width = gray_img.shape
+    gray_img = 255 - gray_img
+    img_mean = np.mean(gray_img)
+    img_std = np.std(gray_img)
+    gray_img[gray_img > img_mean+img_std] = 255
+    gray_img[gray_img <= img_mean+img_std] = 0
+    
+    closing = gray_img
+    image_edges = cv2.Canny(closing, 10, 20)
+
+    [border_locations_x, border_locations_y] = np.where(image_edges == 255)
+    border_locations_x = border_locations_x.astype(np.uint16)
+    border_locations_y = border_locations_y.astype(np.uint16)
+
+    f = DetectOuterBorder()
+    [border_x, border_y] = f.detect_border(border_locations_x, border_locations_y)
+    border_x = border_x[border_x != 0]
+    border_y = border_y[border_y != 0]
+    
+    x0 = border_x[0]
+    y0 = border_y[0]
+    x1 = border_x[1]
+    y1 = border_y[1]
+    rects = detect_rects(border_x, border_y, x0, y0, x1, y1, rgb_img)
+    print(rects)
+    # for x,y in zip(border_x, border_y):
+    #     cv2.circle(rgb_img, (y, x), 1, (0, 0, 255), 1)
     # cv2 rectangle
-    cv2.rectangle(rgb_img, (y0, x0), (y1, x1), (0, 255, 0), 1)
+    for rect in rects:
+        cv2.rectangle(rgb_img, (rect[1], rect[0]), (rect[3], rect[2]), (0, 0, 255), 1)
     cv2.imshow('image', rgb_img)
     cv2.waitKey(0)
 
